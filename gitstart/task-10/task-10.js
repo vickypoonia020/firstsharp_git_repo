e919c215540ed1325c4aebf0e91b17b3f613720b
vicky-poonia <vickypoonia020@gmail.com>
2023-05-21 13:59:57 +0530

    task-10

---
+++ gitstart/task-10/task-10.js
@@ -0,0 +1,28 @@
+let form = document.querySelector("form").addEventListener("submit", myfun);
+function myfun(e) {
+  e.preventDefault();
+  let inputName = document.querySelector("#name");
+  let inputEmail = document.querySelector("#email");
+  let inputNumber = document.querySelector("#number");
+
+  if (
+    inputName.value === "" ||
+    inputEmail.value === "" ||
+    inputNumber.value === ""
+  ) {
+    alert("Please Fill All credentials");
+  } else {
+    let userDetails = {
+      Name: inputName.value,
+      Email: inputEmail.value,
+      Phone: inputNumber.value,
+    };
+
+    localStorage.setItem("userDetails", JSON.stringify(userDetails));
+
+    alert("Registration Done Please Check Local Storage");
+    inputName.value = " ";
+    inputEmail.value = " ";
+    inputNumber.value = " ";
+  }
+}
